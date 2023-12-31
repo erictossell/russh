7f06ec31545601e22c561dd6268f5d84e4db1a86
Eric Tossell <erictossell@gmail.com>
2023-12-31 18:45:34 -0500

    Nix support

--- flake.nix
+++ flake.nix
@@ -1,39 +1,20 @@
 {
-  description = "A very cool Rust application";
-
+  description = "Russh - a simple ssh wrapper";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.russh = pkgs.stdenv.mkDerivation {
-          name = "russh";
-          src = pkgs.lib.cleanSourceWith {
-            src = ./.; # Points to your project directory
-            filter = name: type: 
-              let
-                baseName = baseNameOf name;
-              in
-                !builtins.elem baseName [ "config.json" ]; # Exclude config.json
-          };         
-         
-          cargoSha256 = "0000000000000000000000000000000000000000000000000000";
-          buildInputs = [ pkgs.rustc pkgs.cargo pkgs.gcc ];
-          buildPhase = ''
-            cargo build --release
-          '';
-          installPhase = ''
-            install -D target/release/russh $out/bin/russh
-          '';
-        };
-
-        defaultPackage = self.packages.${system}.russh;
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in {
+      packages = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./default.nix { };
       });
+      devShells = forAllSystems (system: {
+        default = pkgsFor.${system}.callPackage ./shell.nix { };
+      });
+    };
+    
 }
-
